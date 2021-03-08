181851db3124a7568bea1e6829bb58754407aea9
HSA-Student <student.arm.dawodi@horizoncolumbus.org>
2021-03-08 10:35:20 -0500

    yo :)

---
+++ src/org/horizoncolumbus/hs/Main.java
@@ -0,0 +1,27 @@
+package org.horizoncolumbus.hs;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter Row size: ");
+        int rowSize = input.nextInt();
+        System.out.print("Please enter Column size: ");
+        int columnSize = input.nextInt();
+        int[][] matrix = new int[rowSize][columnSize];
+
+        for(int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < columnSize; j++) {
+                System.out.print("Enter Row " + (i+1) + " Column " + (j+1) + " size: ");
+                matrix[i][j] = input.nextInt();
+            }
+        }
+        System.out.println();
+        for(int i = 0; i < rowSize; i++){
+            for(int j = 0; j < columnSize; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    }
